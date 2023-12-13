d4b0b2240646fd56319eec7893fe28dd5a038ec6
maeneak <20371477+maeneak@users.noreply.github.com>
2023-12-13 12:17:54 +1000

    Rename

--- custom_components/ultraloq_ble/const.py
+++ custom_components/ultraloq_ble/const.py
@@ -1,9 +1,9 @@
-"""Constants for integration_blueprint."""
+"""Constants for ultraloq_ble."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Ultraloq BLE"
+DOMAIN = "ultraloq_ble"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
